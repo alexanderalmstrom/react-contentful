603d1143e654020703ec3ce12545f9fcb318e6e6
alexanderalmstrom <info@alexanderalmstrom.com>
2017-11-11 22:35:06 +0100

    Render post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -8,16 +8,38 @@ import './Post.scss'
 
 class Post extends Component {
 	componentWillMount () {
-		
+		this.props.loadEntry('post', this.props.match.slug)
+	}
+
+	renderPost (post) {
+		if (post && post.fields) {
+			return (
+				<div className="post">
+					<img src={post.fields.image.fields.file.url}
+						alt={post.fields.image.fields.title} />
+					<h1>{post.fields.name}</h1>
+					<p>{post.fields.description}</p>
+				</div>
+			)
+		}
 	}
 
 	render () {
+		const post = this.props.posts.entries[this.props.match.params.slug]
+
 		return (
 			<div>
-				Post
+				{this.renderPost(post)}
 			</div>
 		)
 	}
+}
+
+Post.propTypes = {
+	app: PropTypes.object,
+	posts: PropTypes.object,
+	loadEntry: PropTypes.func,
+	params: PropTypes.object
 }
 
 export default connectComponent(Post)
