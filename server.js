8ec650a0b84468c5349354dad0281d5ca49b5ea3
alexanderalmstrom <info@alexanderalmstrom.com>
2017-11-11 23:50:54 +0100

    Fix server paths

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(__dirname))
+app.use(express.static(path.resolve(__dirname, 'build')))
 
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'index.html'))
+	res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 })
 
 app.listen(process.env.PORT || 5000, function () {
-	console.log('Magic happens on %d, in %s mode', (process.env.PORT || 5000), app.get('env'))
+	console.log('Magic happens on port %d in %s mode', (process.env.PORT || 5000), app.get('env'))
 })
